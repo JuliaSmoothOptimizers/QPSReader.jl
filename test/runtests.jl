4a7aefff5675e9c92d47f5fed420941b3cc4f1ce
RenanOD <renan_oclides@hotmail.com>
2019-09-22 20:57:08 -0300

    now downloading with download function

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,16 +4,13 @@ using QPSReader, NLPModels, NLPModelsIpopt, QuadraticModels, LinearOperators
 
 # download qps files
 
-for PROBLEM in ["GENHS28.SIF", "HS76.SIF", "QPTEST.SIF"]
-  run(`wget https://bitbucket.org/optrove/maros-meszaros/raw/9adfb5707b1e0b83a2e0a26cc8310704ff01b7c1/$PROBLEM`)
-end
-
 problems = ["GENHS28.SIF"; "HS76.SIF"; "QPTEST.SIF"]
 objectives = [9.2717369e-01; -4.6818182e+00; 4.3718750e+00]
 
 @testset "qpsreader" begin
   for (k, p) in enumerate(problems)
-    qps = readqps("../$p")
+    p_path = download(https://bitbucket.org/optrove/maros-meszaros/raw/9adfb5707b1e0b83a2e0a26cc8310704ff01b7c1/$p)
+    qps = readqps(p_path)
     qp = QuadraticModel(qps.c, qps.Q, opHermitian(qps.Q), qps.A, qps.lcon, qps.ucon, qps.lvar, qps.uvar, c0=qps.c0)
 
     output = ipopt(qp, print_level=0)
