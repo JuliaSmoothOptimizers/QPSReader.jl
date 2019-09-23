eb5a602f64d3db6da8186ad58526bfd80d1d6daf
RenanOD <renan_oclides@hotmail.com>
2019-09-22 22:49:09 -0300

    put link inside string, temporarily remove Quadraticmodel from require

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ objectives = [9.2717369e-01; -4.6818182e+00; 4.3718750e+00]
 
 @testset "qpsreader" begin
   for (k, p) in enumerate(problems)
-    p_path = download(https://bitbucket.org/optrove/maros-meszaros/raw/9adfb5707b1e0b83a2e0a26cc8310704ff01b7c1/$p)
+    p_path = download("https://bitbucket.org/optrove/maros-meszaros/raw/9adfb5707b1e0b83a2e0a26cc8310704ff01b7c1/$p")
     qps = readqps(p_path)
     qp = QuadraticModel(qps.c, qps.Q, opHermitian(qps.Q), qps.A, qps.lcon, qps.ucon, qps.lvar, qps.uvar, c0=qps.c0)
 
