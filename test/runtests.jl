664d703c0a6a5a8f2e7c71c700c0beb6ffd7a6a2
Dominique Orban <dominique.orban@gmail.com>
2020-05-10 15:00:27 -0400

    provide test collections as Julia artifacts
    artifacts are only available in Julia ≥ 1.3

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,16 @@ using Logging
 
 using QPSReader
 
+if VERSION ≥ v"1.3"
+  netlib_path = fetch_netlib()
+  @test ispath(netlib_path)
+  @test isfile(joinpath(netlib_path, "AFIRO.SIF"))
+  mm_path = fetch_mm()
+  @test ispath(mm_path)
+  @test isfile(joinpath(mm_path, "AUG3D.SIF"))
+end
+
 include("parser.jl")
 include("qp-example.jl")
 include("rimdata.jl")
+
