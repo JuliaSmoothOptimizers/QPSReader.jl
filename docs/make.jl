a0fef5babc7ca76c810c76e713250a6cbf782a14
JSOBot <69814847+JSOBot@users.noreply.github.com>
2021-04-14 21:40:34 -0400

    adding/updating file: docs/make.jl

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter, QPSReader
+
+makedocs(
+  modules = [QPSReader],
+  doctest = true,
+  # linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
+  sitename = "QPSReader.jl",
+  pages = Any["Home" => "index.md",
+              "Tutorial" => "tutorial.md",
+              "Reference" => "reference.md"]
+)
+
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/QPSReader.jl.git")
