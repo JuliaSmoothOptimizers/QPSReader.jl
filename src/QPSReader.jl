0bd6fcda4e887c2af5964b0acd7a29833f049fcb
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-08-16 21:16:23 -0500

    Upgrade QPSReader.jl

--- src/QPSReader.jl
+++ src/QPSReader.jl
@@ -1,31 +1,20 @@
 module QPSReader
 
 export QPSData, readqps
+using LazyArtifacts
 
 include("readqps.jl")
 
-if VERSION ≥ v"1.3"
-  using Pkg.Artifacts
+export fetch_netlib, fetch_mm
 
-  export fetch_netlib, fetch_mm
+"Return the path to the Netlib linear optimization test set."
+function fetch_netlib()
+  return joinpath(artifact"netliblp", "optrove-netlib-lp-f83996fca937")
+end
 
-  "Return the path to the Netlib linear optimization test set."
-  function fetch_netlib()
-    artifact_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
-    ensure_artifact_installed("netliblp", artifact_toml)
-    netliblp_hash = artifact_hash("netliblp", artifact_toml)
-    @assert artifact_exists(netliblp_hash)
-    return joinpath(artifact_path(netliblp_hash), "optrove-netlib-lp-f83996fca937")
-  end
-
-  "Return the path to the Maros-Meszaros quadratic optimization test set."
-  function fetch_mm()
-    artifact_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
-    ensure_artifact_installed("marosmeszaros", artifact_toml)
-    mm_hash = artifact_hash("marosmeszaros", artifact_toml)
-    @assert artifact_exists(mm_hash)
-    return joinpath(artifact_path(mm_hash), "optrove-maros-meszaros-9adfb5707b1e")
-  end
+"Return the path to the Maros-Meszaros quadratic optimization test set."
+function fetch_mm()
+  return joinpath(artifact"marosmeszaros", "optrove-maros-meszaros-9adfb5707b1e")
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,12 @@ using Logging
 
 using QPSReader
 
-if VERSION ≥ v"1.3"
-  netlib_path = fetch_netlib()
-  @test ispath(netlib_path)
-  @test isfile(joinpath(netlib_path, "AFIRO.SIF"))
-  mm_path = fetch_mm()
-  @test ispath(mm_path)
-  @test isfile(joinpath(mm_path, "AUG3D.SIF"))
-end
+netlib_path = fetch_netlib()
+@test ispath(netlib_path)
+@test isfile(joinpath(netlib_path, "AFIRO.SIF"))
+mm_path = fetch_mm()
+@test ispath(mm_path)
+@test isfile(joinpath(mm_path, "AUG3D.SIF"))
 
 include("parser.jl")
 include("qp-example.jl")
